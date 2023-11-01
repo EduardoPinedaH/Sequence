ec314608ddcdade9cb8e4fcc10a54dcac8855e88
EduardoPinedaH <eduardopinedahu@gmail.com>
2023-11-01 15:47:36 -0600

    video hero added

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import HeroOverlay from "../public/Hero.png";
 
 const HomePage = () => {
     return (
@@ -7,12 +8,17 @@ const HomePage = () => {
             id='homepage'
             className='max-container padding-container flex flex-col xl:flex-row'
         >
-            <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
+            <div className='relative flex flex-1 flex-col xl:w-1/2'>
+                <iframe
+                    src='https://player.cloudinary.com/embed/?public_id=sequence%2FDemo&cloud_name=dysfrid9h&player[controls]=false&player[muted]=true&player[floatingWhenNotVisible]=right&player[hideContextMenu]=true&player[autoplayMode]=on-scroll&player[autoplay]=true&player[loop]=true'
+                    className=' w-full h-[865px]'
+                ></iframe>
                 <Image
-                    src='/Hero.png'
+                    src={HeroOverlay}
                     alt='heroimg'
                     width={1920}
                     height={1080}
+                    className='absolute z-[99999]'
                 />
             </div>
         </section>
